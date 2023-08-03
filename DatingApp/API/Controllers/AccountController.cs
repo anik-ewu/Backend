ee6e5e1cb68d2978ba516700438819d239802243
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2023-08-04 00:24:45 +0600

    Add DTO for Register Endpoint

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTO;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,14 +18,17 @@ namespace API.Controllers
         }
 
         [HttpPost("register")] //api/account/register
-        public async Task<ActionResult<AppUser>> Resgister(string userName, string password)
+        public async Task<ActionResult<AppUser>> Resgister(RegisterDto registerDto)
         {
+            if (await UserExist(registerDto.UserName)) 
+            {
+                return BadRequest("Username is taken");
+            }
             using var  hmac = new HMACSHA512();
-            // Getting error for null password
             var user = new AppUser
             {
-                UserName = userName,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -31,6 +36,11 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool>UserExist(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
         
     }
