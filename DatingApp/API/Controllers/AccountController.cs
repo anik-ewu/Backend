366a9e47b818316167d9516e44fbe29d3ecd3394
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2023-08-06 00:16:25 +0600

    Adding a loging endpoint

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -38,6 +38,27 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await  _context.Users.SingleOrDefaultAsync(x => 
+            x.UserName == loginDto.UserName);
+
+            if (user == null) return Unauthorized("Invalid username");
+
+            using var  hmac = new HMACSHA512(user.PasswordSalt);
+
+            var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password
+            ));
+
+            for (int i = 0; i < ComputeHash.Length; i++)
+            {
+                if (ComputeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid passowrd");
+            }
+
+            return user;
+        }
+
         private async Task<bool>UserExist(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
