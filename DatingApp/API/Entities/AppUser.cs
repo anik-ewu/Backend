43796ce59da7c84468a2fff4e461127dbc6e4181
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2023-11-17 23:35:12 +0600

    fix database migration

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -9,11 +9,11 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash {get; set;}
         public byte[] PasswordSalt {get; set;}
-        public DateOnly DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
         public DateTime LastActive { get; set; } = DateTime.UtcNow;
-        public string Genger { get; set; }
+        public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
         public string Interests { get; set; }
