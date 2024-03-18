5774e4b4e91c76303ddd7e3e25b608575809a4ca
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2024-03-19 02:48:58 +0600

    showing user pohoto on nav menu

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -41,15 +41,16 @@ namespace API.Controllers
             return new UserDto
             {
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
             };
         }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x =>
-            x.UserName == loginDto.UserName);
+            var user = await _context.Users
+            .Include(p => p.Photos)
+            .SingleOrDefaultAsync(x =>  x.UserName == loginDto.UserName);
 
             if (user == null) return Unauthorized("Invalid username");
 
@@ -66,7 +67,8 @@ namespace API.Controllers
             return new UserDto
             {
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
--- DatingApp/API/DTO/UserDto.cs
+++ DatingApp/API/DTO/UserDto.cs
@@ -3,6 +3,7 @@ namespace API.DTO
     public class UserDto
     {
         public string UserName { get; set; }
-        public string Token  { get; set; }        
+        public string Token  { get; set; }
+        public string PhotoUrl {get; set; }           
     }
 }
