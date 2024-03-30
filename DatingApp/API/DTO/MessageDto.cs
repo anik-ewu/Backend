7585f620d048b35317adf288b33a85d52c5d6ad9
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2024-03-31 04:14:27 +0600

    add mapper for message

--- DatingApp/API/DTO/MessageDto.cs
+++ DatingApp/API/DTO/MessageDto.cs
@@ -2,16 +2,16 @@ namespace API.DTO
 {
     public class MessageDto
     {
-        public int Id { get; set; }
-        public int SenderId { get; set; }
+        public int Id { get; set; }        
+        public int SenderId { get; set; }        
         public string SenderUsername { get; set; }
         public string SenderPhotoUrl { get; set; }
-        public int RecipientId { get; set; }
-        public string RecipientPhotoUrl { get; set; }
+        public int RecipientId { get; set; }        
         public string RecipientUsername { get; set; }
+        public string RecipientPhotoUrl { get; set; }
 
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }
-        public DateTime MessageSend { get; set; } = DateTime.UtcNow;
+        public DateTime MessageSend { get; set; } = DateTime.UtcNow;        
     }
 }
