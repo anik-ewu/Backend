24c289cf63fb4f8386014845a3e41f2e00f9bb25
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2024-03-31 03:58:46 +0600

    Create interface for message

--- DatingApp/API/Entities/Message.cs
+++ DatingApp/API/Entities/Message.cs
@@ -6,16 +6,16 @@ namespace API.Entities
         public int SenderId { get; set; }        
         public string SenderUsername { get; set; }
         public AppUser Sender { get; set; }
-        public int RecipentId { get; set; }        
-        public string RecipentUsername { get; set; }
-        public AppUser Recipent { get; set; }
+        public int RecipientId { get; set; }        
+        public string RecipientUsername { get; set; }
+        public AppUser Recipient { get; set; }
 
         public string Content { get; set; }
         public DateTime? DateRead { get; set; }
         public DateTime MessageSend { get; set; } = DateTime.UtcNow;
 
         public bool SenderDeleted { get; set; }
-        public bool RecipentDeleted { get; set; }
+        public bool RecipientDeleted { get; set; }
 
     }
 }
