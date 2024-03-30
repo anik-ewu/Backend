ac5eecb31a34a3ddba5d78d10a24dd49403e3214
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2024-03-31 04:50:14 +0600

    Create message controller

---
+++ DatingApp/API/Data/Migrations/20240330224855_updateMessageEntity.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class updateMessageEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_RecipentId",
+                table: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "RecipentUsername",
+                table: "Messages",
+                newName: "RecipientUsername");
+
+            migrationBuilder.RenameColumn(
+                name: "RecipentId",
+                table: "Messages",
+                newName: "RecipientId");
+
+            migrationBuilder.RenameColumn(
+                name: "RecipentDeleted",
+                table: "Messages",
+                newName: "RecipientDeleted");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_RecipentId",
+                table: "Messages",
+                newName: "IX_Messages_RecipientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_RecipientId",
+                table: "Messages",
+                column: "RecipientId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Users_RecipientId",
+                table: "Messages");
+
+            migrationBuilder.RenameColumn(
+                name: "RecipientUsername",
+                table: "Messages",
+                newName: "RecipentUsername");
+
+            migrationBuilder.RenameColumn(
+                name: "RecipientId",
+                table: "Messages",
+                newName: "RecipentId");
+
+            migrationBuilder.RenameColumn(
+                name: "RecipientDeleted",
+                table: "Messages",
+                newName: "RecipentDeleted");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_RecipientId",
+                table: "Messages",
+                newName: "IX_Messages_RecipentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Users_RecipentId",
+                table: "Messages",
+                column: "RecipentId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
