51c0c74c26f95f20a45d1721ffe3317b0a12af61
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2024-04-01 06:20:34 +0600

    optimiging code to fetch message thread

--- DatingApp/client/src/app/members/member-details/member-details.component.ts
+++ DatingApp/client/src/app/members/member-details/member-details.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TabDirective, TabsModule, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { TimeagoModule } from 'ngx-timeago';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 import { MemberMessagesComponent } from '../member-messages/member-messages.component';
+import { MessageService } from 'src/app/_services/message.service';
+import { Message } from 'src/app/_models/message';
 
 @Component({
   selector: 'app-member-details',
@@ -16,14 +18,33 @@ import { MemberMessagesComponent } from '../member-messages/member-messages.comp
   imports: [CommonModule, TabsModule, GalleryModule, TimeagoModule, MemberMessagesComponent]
 })
 export class MemberDetailsComponent implements OnInit {
+  @ViewChild('meberTabs') meberTabs?: TabsetComponent;
+  activeTab?: TabDirective;
   member: Member | undefined;
   images: GalleryItem[] = [];
+  messages: Message[] = [];
 
   constructor(private memberService: MembersService,
+    private messageService: MessageService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.loadMember();
+  }
+
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data;
+    if (this.activeTab.heading == 'Messges') {
+      this.loadMessages();
+    }
+  }
+
+  loadMessages() {
+    if (this.member) {
+      this.messageService.getMessageThread(this.member.userName).subscribe({
+        next: messages => this.messages = messages
+      })
+    }
   }
 
   loadMember() {
@@ -32,7 +53,7 @@ export class MemberDetailsComponent implements OnInit {
     this.memberService.getMember(username).subscribe({
       next: member => {
         this.member = member,
-        member.lastActive = new Date(member.lastActive);
+          member.lastActive = new Date(member.lastActive);
         member.created = new Date(member.created);
         this.getImages();
       }
@@ -42,7 +63,7 @@ export class MemberDetailsComponent implements OnInit {
   getImages() {
     if (!this.member) return;
     for (const photo of this.member?.photos) {
-      this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
+      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
     }
   }
 
