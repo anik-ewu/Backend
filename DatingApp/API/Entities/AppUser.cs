d2da89a3bc46c03418dbd4eda2bb99c8c652a78f
Sabbir Hasan Anik <sabbirhasananik@gmail.com>
2024-04-16 05:03:15 +0600

    Start using IdentityUser

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -1,14 +1,11 @@
 
 using API.Extentions;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Entities
 {
-    public class AppUser 
+    public class AppUser: IdentityUser<int> 
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public byte[] PasswordHash {get; set;}
-        public byte[] PasswordSalt {get; set;}
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
